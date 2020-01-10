7b1718d030d713a1021eab4d7a41eec9f62f6d8b
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2020-01-11 01:37:56 +0800

    修正代码

--- src/core/authenticator.cpp
+++ src/core/authenticator.cpp
@@ -100,7 +100,7 @@ void Authenticator::record(const std::string &password, uint64_t download, uint6
             return;
         }
         //上报流量记录处理        
-        if (mysql_query(&con, ("insert into  user_traffic_log (`user_id`, `u`, `d`, `node_id`, `rate`, `traffic`, `log_time`) VALUES (1,"+ to_string(trafficInfo.upload*conf.rate) +","+ to_string(trafficInfo.download * conf.rate) +","+to_string(conf.server_id) +" , "+ to_string(conf.rate) +", "+ to_string((trafficInfo.download+trafficInfo.upload) * conf.rate) +",unix_timestamp() )").c_str())) {
+        if (mysql_query(&con, ("insert into  user_traffic_log (`user_id`, `u`, `d`, `node_id`, `rate`, `traffic`, `log_time`) VALUES (1,"+ to_string(trafficInfo.upload*conf.rate) +","+ to_string(trafficInfo.download * conf.rate) +","+to_string(conf.server_id) +" , "+ to_string(conf.rate) +", "+ Authenticator::traffic_format(()((trafficInfo.download+trafficInfo.upload)*conf.rate)) +",unix_timestamp() )").c_str())) {
             Log::log_with_date_time(mysql_error(&con), Log::ERROR);
         }
         //更新缓存
@@ -114,8 +114,8 @@ void Authenticator::record(const std::string &password, uint64_t download, uint6
             trafficInfo.skip = 1;
             trafficInfoMap[password] = trafficInfo;
         }else{
-            //上报流量记录处理            ;
-            if (mysql_query(&con, ("insert into  user_traffic_log (`user_id`, `u`, `d`, `node_id`, `rate`, `traffic`, `log_time`) VALUES (1,"+ to_string(upload * conf.rate) +","+ to_string(download * conf.rate) +","+to_string(conf.server_id) +" , "+ to_string(conf.rate) +", "+ to_string((download+upload)*conf.rate) +",unix_timestamp() )").c_str())) {
+            //上报流量记录处理             
+            if (mysql_query(&con, ("insert into  user_traffic_log (`user_id`, `u`, `d`, `node_id`, `rate`, `traffic`, `log_time`) VALUES (1,"+ to_string(upload * conf.rate) +","+ to_string(download * conf.rate) +","+to_string(conf.server_id) +" , "+ to_string(conf.rate) +", "+ Authenticator::traffic_format(()((download+upload)*conf.rate)) +",unix_timestamp() )").c_str())) {
                 Log::log_with_date_time(mysql_error(&con), Log::ERROR);
             }
         }       
@@ -137,6 +137,16 @@ bool Authenticator::is_valid_password(const std::string &password) {
     return true;
 }
 
+static string Authenticator::traffic_format(uint64_t traffic) { 
+    if (traffic < 1024 * 8){
+        return to_string(traffic) + "B";
+    }     
+    if (traffic < 1024 * 1024 * 2){
+        return to_string( setiosflags(ios::fixed)<<setprecision(2)<< (traffic/ 1024.0)) + "KB";
+    }           
+    return to_string(setiosflags(ios::fixed)<<setprecision(2)<< (traffic / 1048576.0) ) + "MB";
+}
+
 Authenticator::~Authenticator() {
     mysql_close(&con);
 }
@@ -147,7 +157,7 @@ Authenticator::Authenticator(const Config&) {}
 bool Authenticator::auth(const string&) { return true; }
 void Authenticator::record(const std::string&, uint64_t, uint64_t) {}
 bool Authenticator::is_valid_password(const std::string&) { return true; }
-string Authenticator::traffic_format(uint64_t) { return null; }
+static string Authenticator::traffic_format(uint64_t) { return null; }
 Authenticator::~Authenticator() {}
 
 #endif // ENABLE_MYSQL
